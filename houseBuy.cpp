76a51629c56498d4317762daa33d5302b579b76f
FanJiaoJiao <FanJiaoJiao@users.noreply.github.com>
2016-04-08 16:41:51 +0800

    Update houseBuy.cpp

--- houseBuy.cpp
+++ houseBuy.cpp
@@ -3,25 +3,20 @@ using namespace std;
 int main()  
 {  
     int N;
-	double K;  
+    double K;  
     double price = 200;    
     int year =1;  
 	cin>>N>>K;
-
-        while( year*N<price )  
-        {   
-	    price = price*(1.0+K/100);  
-		year++;
-		if(year>20)
-			break;
-}
-
-            if(year>20)  
-                
-                cout<<"Impossible"<<endl; 
-           
-        else if(year<=20)  
-		cout<<year<<endl;
-		
+    while( year*N<price )  
+    {   
+        price = price*(1.0+K/100);  
+        year++;
+	if(year>20)
+	break;
+    }
+    if(year>20)  
+       cout<<"Impossible"<<endl; 
+    else if(year<=20)  
+       cout<<year<<endl;
     return 0;  
 }  
